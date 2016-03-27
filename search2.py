bad2e0892720a4b2cfa6c4d0df1f3b9795cdc616
ibechane <ibechane@gmail.com>
2016-03-27 13:18:38 -0400

    took into account sequential ops

--- search2.py
+++ search2.py
@@ -1,4 +1,6 @@
 from __future__ import division
+import re
+
 
 def find_paths(src, target):
     """ Loop through possible expressions and check if they equal the target """
@@ -7,32 +9,51 @@ def find_paths(src, target):
     num_insert_combos = len(inserts) ** (len(src) - 1)
     good_paths = []
     for combo in xrange(num_insert_combos):
-        insert = get_base_five(combo).zfill(len(src) - 1)
+        insert = change_base(combo, len(inserts)).zfill(len(src) - 1)
         path = []
         for i in xrange(len(src)):
-            path.append(src[i]) # append a digit
-            try:
+            path.append(src[i])  # append a digit
+            if i < len(insert):
                 operation = inserts[int(insert[i])]
                 path.append(operation)
-            except IndexError:
-                pass
         path = ''.join(path)
-        result = eval(path)
+        result = evaluate_sequentially(path)
+        # print("%s = %s" % (path, result))
         if result == target:
             expression = "%s = %s" % (path, target)
             good_paths.append(expression)
     return good_paths
 
-def get_base_five(num):
+
+def change_base(num, base):
     """ Takes in base-10 number and returns base-5 representation as string """
+    if base > 10 or base < 2 or type(base) is not int:
+        raise Exception("Only supports integer bases from 2 - 10")
+    if base == 10:
+        return num
     digits = []
     while num != 0:
-        digits.append(num % 5)
-        num //= 5
-    return ''.join(map(str,digits))[::-1]
+        digits.append(num % base)
+        num //= base
+    return ''.join(map(str, digits))[::-1]
+
+
+def evaluate_sequentially(path):
+    """ Takes a mathematical expression without parens and performs the operations sequentially, 
+        ignoring normal order of operations """
+    ops = re.findall(r'\W?\d+', path)  # returns operation pieces, e.g. ['3', '*1', '/41']
+    result = ops[0]
+    for op in ops[1:]:
+        try:
+            result = eval(str(result) + op)
+        except ZeroDivisionError:
+            result = 'NaN'
+            break
+    return result
+
 
 if __name__ == "__main__":
-    src = 3141
-    target = 12
-    paths = find_paths(src, target)
-    print("\n".join(paths))
+    my_src = 3141
+    my_target = 12
+    my_paths = find_paths(my_src, my_target)
+    print("\n".join(my_paths))
